408cb02f935705795ec48a1fa629d8504d7b16bd
Andres Suarez <andres.suarez@fastdevelopments.net>
2024-05-15 21:31:29 -0500

    cambio en pantalla Home

--- obj/Debug/net8.0/artesania.AssemblyInfo.cs
+++ obj/Debug/net8.0/artesania.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("artesania")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8b406c7275846178b878f32e35fce3ee8a49b00")]
 [assembly: System.Reflection.AssemblyProductAttribute("artesania")]
 [assembly: System.Reflection.AssemblyTitleAttribute("artesania")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
